d1ccd4cf867851f20f0aab7523d49b62397a9f5d
devRetorcido <mesadir6b@gmail.com>
2024-05-25 23:05:57 -0400

    uso de la libreria beatifullsoup

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
+
+#from selenium import webdriver
+#from webdriver_manager.chrome import ChromeDriverManager
+
+#instalar el webdriver de chrome
+#driver = webdriver.Chrome(ChromeDriverManager().install())
+
+#navegar a website
+#driver.get ("https://bo.skokka.com/escorts/santa-cruz/")
+
+# cerrar navegador y sesonp
+#driver.quit()
+#time.sleep(10)
+
+
 import re
 #from colorama import Fore
 import requests
+import time
+
+Website ="https://bo.skokka.com/escorts/santa-cruz/"
 
 
-Website ='https://bo.skokka.com/escorts/santa-cruz/'
 resultado = requests.get(Website)
+cabecera = resultado.headers["content-type"]
 content = resultado.text
-print(content)
+print(cabecera)
+print("*",30)
+#print(content)
+
+
