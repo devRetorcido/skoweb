56d7c2d17af391707a6545fdb98b0d91f960607d
devRetorcido <mesadir6b@gmail.com>
2024-05-27 09:12:02 -0400

    Se agrega nueva liberira  useragent

--- main.py
+++ main.py
@@ -15,17 +15,51 @@
 
 import re
 #from colorama import Fore
+from bs4 import BeautifulSoup
 import requests
+from requests.exceptions import HTTPError
 import time
+from fake_useragent import UserAgent
 
-Website ="https://bo.skokka.com/escorts/santa-cruz/"
+ua = UserAgent()
+#Actualiza otro navegador
+#ua.update()
+
+headers = {"User-Agent": ua.random}
 
 
-resultado = requests.get(Website)
-cabecera = resultado.headers["content-type"]
+try:
+
+    Website ="https://bo.skokka.com/escorts/santa-cruz/"
+    resultado = requests.get(Website,headers=headers)
+    resultado.raise_for_status()
+    #print(resultado.text)
+
+except HTTPError as http_err:
+    print('Errpr HTTP: {}'.format(http_err))
+except Exception as err:
+    print('Error  {}'.format(err))
+
+#cabecera = resultado.headers["content-type"]
 content = resultado.text
-print(cabecera)
-print("*",30)
+#print (content)
+soup = BeautifulSoup(content,"html.parser")
+
+#texto_h2 = soup.find('h2')
+#print(texto_h2.text)
+
+
+
+#h2_todos = soup.find_all("h2",class_="listing-title item-title")
+div_todos = soup.find_all('div',class_="item-image-supertop")
+
+#><img src="https://bo-static.imgskk.com/post/7b/f2/7bf26c66390e4deb9160045e36834585.jpg?listing=supertop&amp;v=84ssm3xe" alt="HERMOSA MUJER 😍 DISPONIBLE A CLIENTES EXIGENTES!" class="avatar"></div>)
+print (div_todos)
+for div in  div_todos:
+    imagen = soup.find('v-lazy-image alt')
+    print(imagen.text)
+
+    
 #print(content)
 
 
